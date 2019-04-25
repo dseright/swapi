1c074f10a0c31df51c8c446227452efb4e82a848
Dustin Seright <dseright@gmail.com>
2019-04-25 01:28:07 -0500

    Next and previous page functions, might need to tidy them up

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Table from '../components/Table'
 
+/* Okay, so I've got pagination working. Now I need to add the ability to change between the different choices (people, vehicles, etc...) and then 
+only display the first couple of tidbits on the table, but make it possible to click on the name of the thing in question and bring up a single page for it 
+displaying all the info on the database. Then it'll be fully navigatable. Also I need to style it at some point. */
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      people: {}
+      people: {},
+      next: '',
+      prev: ''
     }
   }
 
@@ -14,14 +20,23 @@ class App extends Component {
     this.getData();
   }
 
-  getData = (data = 'people/') => {
-    let url = 'https://swapi.co/api/' + data;
-    console.log(url);
-    fetch(url).then(response => {
+  getData = (data = 'https://swapi.co/api/people/') => {
+    fetch(data).then(response => {
       return response.json();
     }).then(response => {
-      this.setState({ people: response})
-    })
+      this.setState({ people: response});
+      this.setState({ next: response.next});
+      this.setState({ prev: response.previous});
+      console.log(response);
+    })  
+  }
+
+  nextPage = () => {
+    this.getData(this.state.next);
+  }
+
+  prevPage = () => {
+    this.getData(this.state.prev);
   }
 
   render() {
@@ -32,6 +47,8 @@ class App extends Component {
       return (
         <div className="App">
           <Table people={people}/>
+          <button onClick={this.prevPage}>Previous</button>
+          <button onClick={this.nextPage}>Next</button>
         </div>
       );
     }
