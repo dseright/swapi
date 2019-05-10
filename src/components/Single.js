13c087e3f7a40c25ade402ad9b09290d49d6ec82
Dustin Seright <dseright@gmail.com>
2019-05-09 19:54:15 -0500

    Linkage working, just need to fix buttons and style

--- src/components/Single.js
+++ src/components/Single.js
@@ -8,12 +8,17 @@ class Single extends Component {
     console.log(data);
     return(
         data.map(([key, value], i) => {
-          let newValue = '';
-          if ((typeof value === 'string' || value instanceof String) && value.startsWith('https')) {
-            newValue = <button onClick={() => changeData(value)}>{value}</button>;
+          let newValue = [];
+          if (Array.isArray(value)) {
+            value.forEach((link, j) => {
+              newValue.push(<button key ={j} onClick={() => changeData(link)}>{link}</button>)
+            })
+          } else if ((typeof value === 'string' || value instanceof String) && value.startsWith('https')) {
+            newValue.push(<button key={i} onClick={() => changeData(value)}>{value}</button>);
           } else {
-            newValue = value;
+            newValue.push(value);
           }
+          console.log(newValue);
           return <p key={i}>{key}: {newValue}</p>
       })
     )
